5ccd4de8ea3723fb9fbb4c1ceba7f66880755316
lee-aaron <lee.aaron.68@gmail.com>
2017-07-26 23:04:46 -0700

    Added more features

--- WebUpdate/README.md
+++ WebUpdate/README.md
@@ -1,11 +1,14 @@
 # WebUpdate
 
-A script that will run and check updates for manga.
+A script that will run and check updates for manga on mangareader.net.
 
 ## Ver 1.0.0:
 
-Supports downloading multiple manga chapters or all for multiple manga.
+* Supports downloading multiple manga chapters or all for multiple manga.
+* Doesn't duplicate manga downloads.
+* Deletes manga.
 
 ## Planned Updates:
 
-Check if manga is already entered and if there are any new updates.
+* Check for and download new updates to manga.
+* Incorporate other websites like mangareader.co.
--- WebUpdate/webupdate.sh
+++ WebUpdate/webupdate.sh
@@ -8,121 +8,156 @@ index()
 [ ! -d $(pwd)/Manga ] && mkdir -p ./Manga
 [ ! -f $(pwd)/Manga/manga.txt ] && touch ./Manga/manga.txt
 
-while true; do
-	echo "Please enter the full name of the manga (ex. One Piece or Kono Subarashii Sekai Ni Shukufuku O). Type Done or done if you don't want to enter anymore."
-	read resp
-	if [[ "$resp" == "Done" || "$resp" == "done" ]]; then
-		break
-	else
-		resp=${resp// /-}
-		[ ! -d $(pwd)/Manga/$resp ] && mkdir -p ./Manga/$resp
-		echo "$resp" >> ./Manga/manga.txt
-	fi
+PS3='Type a number corresponding to option: '
+options=("Download Manga" "Check & Download Updates" "Delete Manga" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+		"Download Manga")
+			while true; do
+				echo "Please enter the full name of the manga (ex. One Piece or Kono Subarashii Sekai Ni Shukufuku O). Type Done or done if you don't want to enter anymore."
+				read resp
+				if [[ "$resp" == "Done" || "$resp" == "done" ]]; then
+					break
+				else
+					resp=${resp// /-}
+					[ ! -d $(pwd)/Manga/$resp ] && mkdir -p ./Manga/$resp
+					if grep -Fxq $resp ./Manga/manga.txt; then
+						echo "Manga is already entered"
+					else
+						echo "$resp" >> ./Manga/manga.txt
+					fi
+				fi
+			done
+			cd Manga
+			while IFS='' read -r line <&9 || [[ -n "$line" ]]; do
+				cd $line
+				manga=$line
+				manga=${manga//-/ }
+				echo "Type Y/y to download all chapters for $manga or type 0 to skip or type anything else to select a range"
+				read -r ans
+				if [[ $ans == "Y" || $ans == "y" ]]; then
+					chaps=1
+					while true; do
+						[ ! -d ./$chaps ] && mkdir -p ./$chaps
+						cd $chaps
+						line=${line,,}
+						wget -q -O index.html -c www.mangareader.net/$line/$chaps
+						if grep -q "not released yet" index.html; then
+							cd ..
+							rm -rf $chaps
+							break
+						fi
+						rm index.html
+						declare -i i=1
+						while true; do
+							echo "Downloading page $i of Ch. $chaps"
+							wget -q -O $i.html -c www.mangareader.net/$line/$chaps/$i
+							grep 'src=\"http' $i.html | grep 'mangareader' > jump.txt
+							link=$(head -n 1 jump.txt)
+							starti=$(index $link "11q")
+							endi=$(index $link "12q")
+							if grep -q "Larger Image" $i.html; then
+								starti=$(index $link "9q")
+								endi=$(index $link "10q")
+							fi
+							length=$((endi-starti))
+							image=${link:$((starti+1)):$((length-1))}
+							length=${#image}
+							if [[ "$length" -eq 0 ]]; then
+								break
+							fi
+							imagename=0000$i
+							wget -O ${imagename: -4}.jpg -q -c $image
+							i=i+1
+						done
+						echo "Converting to pdf..."
+						chapno=0000$chaps
+						chapno=${chapno: -4}
+						convert *.jpg ../Ch$chapno.pdf
+						echo "Cleaning up....."
+						path=$(pwd)
+						echo -e "Your downloaded file is in this path:\n" $path
+						cd ..
+						rm -rf $chaps
+						chaps=chaps+1
+					done
+					cd ..
+				elif [ $ans == "0" ]; then
+					cd ..
+					continue
+				else
+					echo "Enter the chapter range."
+					echo "Start:"
+					read chaps
+					echo "End:"
+					read chape
+					for((c=$chaps;c<=$chape;c++)) do
+						[ ! -d ./$chaps ] && mkdir -p ./$chaps
+						cd $chaps
+						line=${line,,}
+						wget -q -O index.html -c www.mangareader.net/$line/$chaps
+						if grep -q "not released yet" index.html; then
+							echo "Ch. $chaps of $line  is not available at www.mangareader.net"
+							cd ..
+							rm -rf $chaps
+							break
+						fi
+						rm index.html
+						declare -i i=1
+						while true; do
+							echo "Downloading page $i of Ch. $chaps"
+							wget -q -O $i.html -c www.mangareader.net/$line/$chaps/$i
+							grep 'src=\"http' $i.html | grep 'mangareader' > jump.txt
+							link=$(head -n 1 jump.txt)
+							starti=$(index $link "11q")
+							endi=$(index $link "12q")
+							if grep -q "Larger Image" $i.html; then
+								starti=$(index $link "9q")
+								endi=$(index $link "10q")
+							fi
+							length=$((endi-starti))
+							image=${link:$((starti+1)):$((length-1))}
+							length=${#image}
+							if [[ "$length" -eq 0 ]]; then
+								break
+							fi
+							imagename=0000$i
+							wget -O ${imagename: -4}.jpg -q -c $image
+							i=i+1
+						done
+						echo "Converting to pdf..."
+						chapno=0000$chaps
+						chapno=${chapno: -4}
+						convert *.jpg ../Ch$chapno.pdf
+						echo "Cleaning up....."
+						cd ..
+						path=$(pwd)
+						echo -e "Your downloaded file is in this path:\n" $path
+						rm -rf $chaps
+					done
+					cd ..
+				fi
+			done 9< "manga.txt"
+			echo "Downloads complete. Please use 'gnome-open' to open the PDFs."	
+			;;
+		"Check & Download Updates")
+			echo "In Progress"
+			;;
+		"Delete Manga")
+			echo "Which manga would you like to delete? (ex. One Piece or Kono Subarashii Sekai Ni Shukufuku O)"
+			read resp
+			resp=${resp// /-}
+			if [ -d  ./$resp ]; then
+				rm -r ./$resp
+				echo "Manga deleted"			
+			else
+				echo "Manga doesn't exist"				
+			fi
+			;;
+		"Quit")
+			break
+			;;
+		*) echo Invalid Option;;
+	esac
 done
-cd Manga
-while IFS='' read -r line <&9 || [[ -n "$line" ]]; do
-	cd $line
-	manga=$line
-	manga=${manga//-/ }
-	echo "Do you want to download all the chapters for $manga? Type Y/y"
-	read -r ans
-	if [[ $ans == "Y" || $ans == "y" ]]; then
-		chaps=1
-		while true; do
-			[ ! -d ./$chaps ] && mkdir -p ./$chaps
-			cd $chaps
-			line=${line,,}
-			wget -q -O index.html -c www.mangareader.net/$line/$chaps	
-			if grep -q "not released yet" index.html; then
-				cd ..
-				rm -rf $chaps
-				break
-			fi
-			rm index.html
-			declare -i i=1
-			while true; do
-				echo "Downloading page $i of Ch. $chaps"
-				wget -q -O $i.html -c www.mangareader.net/$line/$chaps/$i
-				grep 'src=\"http' $i.html | grep 'mangareader' > jump.txt
-				link=$(head -n 1 jump.txt)
-				starti=$(index $link "11q")
-				endi=$(index $link "12q")
-				if grep -q "Larger Image" $i.html; then
-					starti=$(index $link "9q")
-					endi=$(index $link "10q")
-				fi
-				length=$((endi-starti))
-				image=${link:$((starti+1)):$((length-1))}
-				length=${#image}
-				if [[ "$length" -eq 0 ]]; then
-					break
-				fi
-				imagename=0000$i
-				wget -O ${imagename: -4}.jpg -q -c $image
-				i=i+1	
-			done
-			echo "Converting to pdf..."
-			chapno=0000$chaps
-			chapno=${chapno: -4}
-			convert *.jpg ../Ch$chapno.pdf
-			echo "Cleaning up....."
-			path=$(pwd)
-			echo -e "Your downloaded file is in this path:\n" $path
-			cd ..
-			rm -rf $chaps
-			chaps=chaps+1
-		done
-	else	
-		echo "Enter the chapter range."
-		echo "Start:"
-		read chaps
-		echo "End:"
-		read chape
-		for((c=$chaps;c<=$chape;c++)) do
-			[ ! -d ./$chaps ] && mkdir -p ./$chaps
-			cd $chaps
-			line=${line,,}
-			wget -q -O index.html -c www.mangareader.net/$line/$chaps	
-			if grep -q "not released yet" index.html; then
-				echo "Ch. $chaps of $line  is not available at www.mangareader.net"
-				cd ..
-				rm -rf $chaps
-				break
-			fi
-			rm index.html
-			declare -i i=1
-			while true; do
-				echo "Downloading page $i of Ch. $chaps"
-				wget -q -O $i.html -c www.mangareader.net/$line/$chaps/$i
-				grep 'src=\"http' $i.html | grep 'mangareader' > jump.txt
-				link=$(head -n 1 jump.txt)
-				starti=$(index $link "11q")
-				endi=$(index $link "12q")
-				if grep -q "Larger Image" $i.html; then
-					starti=$(index $link "9q")
-					endi=$(index $link "10q")
-				fi
-				length=$((endi-starti))
-				image=${link:$((starti+1)):$((length-1))}
-				length=${#image}
-				if [[ "$length" -eq 0 ]]; then
-					break
-				fi
-				imagename=0000$i
-				wget -O ${imagename: -4}.jpg -q -c $image
-				i=i+1	
-			done
-			echo "Converting to pdf..."
-			chapno=0000$chaps
-			chapno=${chapno: -4}
-			convert *.jpg ../Ch$chapno.pdf
-			echo "Cleaning up....."
-			cd ..
-			path=$(pwd)
-			echo -e "Your downloaded file is in this path:\n" $path
-			rm -rf $chaps
-		done
-	fi
-	cd ..
-done 9< "manga.txt"
-echo "Downloads complete. Please use 'gnome-open' to open the PDFs."
