768c713c3da9fe39a05f82e520e3a9c07360a907
lee-aaron <lee.aaron.68@gmail.com>
2017-08-07 19:45:17 -0700

    Test

--- JSTesting/roll.js
+++ JSTesting/roll.js
@@ -1,11 +1,6 @@
 while(true) {
 	if(document.getElementById("time_remaining")) {
-		var div = document.getElementById("time_remaining");
-		var spans = div.getElementsByTagName("span");
-		var minutes = parseInt(spans[2].textContent) * 60;
-		var seconds = parseInt(spans[4].textContent);
-		var totalTime = +minutes + +seconds;
-		console.log(totalTime);
+		
 	} else {
 		document.getElementById("free_play_form_button").click();	
 	}
