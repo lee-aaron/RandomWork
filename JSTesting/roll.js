098a3838b20b961ffd7603d1a9fd9f1243aefec9
lee-aaron <lee.aaron.68@gmail.com>
2017-08-07 19:56:07 -0700

    test

--- JSTesting/roll.js
+++ JSTesting/roll.js
@@ -1,21 +1,20 @@
 function sleep(seconds) {
-	var start = new Date().getTime();
-	for(var i = 0; i < 1e7; i++) {
-		if((new Date().getTime() - start) > seconds*60) {
-			break;		
-		}	
+	return new Promise(resolve => setTimeout(resolve, seconds*60));
+}
+
+async function init() {
+	while(true) {
+		if(document.getElementById("time_remaining")) {
+			var div = document.getElementById("time_remaining");
+			var spans = div.getElementsByTagName("span");
+			var minutes = parseInt(spans[2].textContent) * 60;
+			var seconds = parseInt(spans[4].textContent);
+			var totalTime = Number(minutes) + Number(seconds);
+			await sleep(totalTime);
+		} else {
+			document.getElementById("free_play_form_button").click();	
+		}
 	}
 }
 
-while(true) {
-	if(document.getElementById("time_remaining")) {
-		var div = document.getElementById("time_remaining");
-		var spans = div.getElementsByTagName("span");
-		var minutes = parseInt(spans[2].textContent) * 60;
-		var seconds = parseInt(spans[4].textContent);
-		var totalTime = Number(minutes) + Number(seconds);
-		sleep(totalTime);
-	} else {
-		document.getElementById("free_play_form_button").click();	
-	}
-}
+init();
